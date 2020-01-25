c11cde5a244974faac89e48d5d7cfb193774afee
viniciuscavalheiro1 <viniciuspbu@gmail.com>
2020-01-24 23:55:58 -0300

    Automação de execução

--- modulo02/src/routes.js
+++ modulo02/src/routes.js
@@ -1,9 +1,8 @@
-const { Router } = require("express");
+import { Router } from "express";
 
 const routes = new Router();
 
 routes.get("/", (req, res) => {
-  return res.json({ message: "Hello Word" });
+  return res.json({ message: "Hello Word!!!" });
 });
-
-module.exports = routes;
+export default routes;
