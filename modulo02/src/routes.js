640a6ea990fd6394f3d2fc0704f48e551d72b24a
viniciuscavalheiro1 <viniciuspbu@gmail.com>
2020-01-28 01:13:13 -0300

    Configuraçoes de ambiente, bibliotecas e padrões

--- modulo02/src/routes.js
+++ modulo02/src/routes.js
@@ -1,8 +1,6 @@
-import { Router } from "express";
+import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get("/", (req, res) => {
-  return res.json({ message: "Hello Word!!!" });
-});
+routes.get('/', (req, res) => res.json({ message: 'Hello Word!!!' }));
 export default routes;
